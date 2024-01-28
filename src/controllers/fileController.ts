ae31bf4ebc236d2bd5a5b003d7428b4b5a1e73e7
Karo8870 <cazacuchristian@gmail.com>
2024-01-28 23:20:01 +0200

    added try catch in file controller

--- src/controllers/fileController.ts
+++ src/controllers/fileController.ts
@@ -2,9 +2,14 @@ import { Request, Response } from '../modules/controllerModule';
 import path from 'path';
 
 const getFile = (req: Request, res: Response) => {
-	res.sendFile(path.resolve(`./multer/uploads/${req.params.path}`), (e) => {
+	try {
+		res.sendFile(path.resolve(`./multer/uploads/${req.params.path}`), (e) => {
+			res.status(500).json(e);
+		});
+	} catch (e) {
+		console.log(e);
 		res.status(500).json(e);
-	});
+	}
 };
 
 export default { getFile };
