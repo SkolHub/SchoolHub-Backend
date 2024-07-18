7a2f96cc16d8266853a768bbf0656f23605702f1
Karo8870 <cazacuchristian@gmail.com>
2024-07-18 20:26:40 +0300

    Fixed absences being grades

--- src/modules/absence/student-absence/student-absence.service.ts
+++ src/modules/absence/student-absence/student-absence.service.ts
@@ -1,39 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { DBService } from '../../../common/db.service';
-import { grades } from '../../../database/schema/grades';
 import { and, eq, sql } from 'drizzle-orm';
 import { members } from '../../../database/schema/members';
+import { absences } from '../../../database/schema/absences';
 
 @Injectable()
 export class StudentAbsenceService extends DBService {
 	findAll() {
 		return this.db
 			.select({
-				subjectID: grades.subjectID,
-				grades: sql`JSONB_AGG
-                (JSONB_BUILD_OBJECT('id', ${grades.id}, 'teacher', JSON_BUILD_OBJECT('id', ${members.id}, 'name', ${members.name}), 'timestamp', ${grades.timestamp}, 'date', ${grades.date}, 'reason', ${grades.reason}, 'value', ${grades.value}))`
+				subjectID: absences.subjectID,
+				absences: sql`JSONB_AGG
+                (JSONB_BUILD_OBJECT('id', ${absences.id}, 'teacher', JSON_BUILD_OBJECT('id', ${members.id}, 'name', ${members.name}), 'timestamp', ${absences.timestamp}, 'date', ${absences.date}, 'reason', ${absences.reason}, 'excused', ${absences.excused}))`
 			})
-			.from(grades)
-			.leftJoin(members, eq(members.id, grades.teacherID))
-			.where(eq(grades.studentID, this.userID))
-			.groupBy(grades.subjectID);
+			.from(absences)
+			.leftJoin(members, eq(members.id, absences.teacherID))
+			.where(eq(absences.studentID, this.userID))
+			.groupBy(absences.subjectID);
 	}
 
 	async findOne(subjectID: number) {
 		return this.db
 			.select({
-				id: grades.id,
-				reason: grades.reason,
+				id: absences.id,
+				reason: absences.reason,
 				teacher: sql`JSON_BUILD_OBJECT
                     ('id', ${members.id}, 'name', ${members.name})`,
-				value: grades.value,
-				timestamp: grades.timestamp,
-				date: grades.date
+				excused: absences.excused,
+				timestamp: absences.timestamp,
+				date: absences.date
 			})
-			.from(grades)
-			.leftJoin(members, eq(members.id, grades.teacherID))
+			.from(absences)
+			.leftJoin(members, eq(members.id, absences.teacherID))
 			.where(
-				and(eq(grades.studentID, this.userID), eq(grades.subjectID, subjectID))
+				and(
+					eq(absences.studentID, this.userID),
+					eq(absences.subjectID, subjectID)
+				)
 			);
 	}
 }
--- src/modules/grade/student-grade/student-grade.service.ts
+++ src/modules/grade/student-grade/student-grade.service.ts
@@ -2,41 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { DBService } from '../../../common/db.service';
 import { and, eq, sql } from 'drizzle-orm';
 import { members } from '../../../database/schema/members';
-import { absences } from '../../../database/schema/absences';
+import { grades } from '../../../database/schema/grades';
 
 @Injectable()
 export class StudentGradeService extends DBService {
 	findAll() {
 		return this.db
 			.select({
-				subjectID: absences.subjectID,
-				absences: sql`JSONB_AGG
-                (JSONB_BUILD_OBJECT('id', ${absences.id}, 'teacher', JSON_BUILD_OBJECT('id', ${members.id}, 'name', ${members.name}), 'timestamp', ${absences.timestamp}, 'date', ${absences.date}, 'reason', ${absences.reason}, 'excused', ${absences.excused}))`
+				subjectID: grades.subjectID,
+				grades: sql`JSONB_AGG
+                (JSONB_BUILD_OBJECT('id', ${grades.id}, 'teacher', JSON_BUILD_OBJECT('id', ${members.id}, 'name', ${members.name}), 'timestamp', ${grades.timestamp}, 'date', ${grades.date}, 'reason', ${grades.reason}, 'value', ${grades.value}))`
 			})
-			.from(absences)
-			.leftJoin(members, eq(members.id, absences.teacherID))
-			.where(eq(absences.studentID, this.userID))
-			.groupBy(absences.subjectID);
+			.from(grades)
+			.leftJoin(members, eq(members.id, grades.teacherID))
+			.where(eq(grades.studentID, this.userID))
+			.groupBy(grades.subjectID);
 	}
 
 	async findOne(subjectID: number) {
 		return this.db
 			.select({
-				id: absences.id,
-				reason: absences.reason,
+				id: grades.id,
+				reason: grades.reason,
 				teacher: sql`JSON_BUILD_OBJECT
                     ('id', ${members.id}, 'name', ${members.name})`,
-				excused: absences.excused,
-				timestamp: absences.timestamp,
-				date: absences.date
+				value: grades.value,
+				timestamp: grades.timestamp,
+				date: grades.date
 			})
-			.from(absences)
-			.leftJoin(members, eq(members.id, absences.teacherID))
+			.from(grades)
+			.leftJoin(members, eq(members.id, grades.teacherID))
 			.where(
-				and(
-					eq(absences.studentID, this.userID),
-					eq(absences.subjectID, subjectID)
-				)
+				and(eq(grades.studentID, this.userID), eq(grades.subjectID, subjectID))
 			);
 	}
 }
