21b8717d2b072fe269db5e065a60e987626525a6
Karo8870 <77077774+Karo8870@users.noreply.github.com>
2024-07-23 11:01:43 +0300

    Update grades.ts

--- src/database/schema/grades.ts
+++ src/database/schema/grades.ts
@@ -3,7 +3,7 @@ import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 export const grades = pgTable('Grade', {
 	id: serial('id').primaryKey(),
 	value: text('value').notNull(),
-	reason: text('reason').notNull(),
+	reason: text('reason'),
 	date: timestamp('date', { mode: 'string' }).notNull(),
 	timestamp: timestamp('timestamp').defaultNow(),
 	studentID: integer('studentID').notNull(),
